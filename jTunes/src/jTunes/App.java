504dc409ff6eef693115961ccf8c309fd5e000ea
PowerstarL2 <joshua.chu@macaulay.cuny.edu>
2016-05-09 21:32:29 -0400

    First try with GroupLayout
    Laid out the main application with GroupLayout

--- jTunes/src/jTunes/App.java
+++ jTunes/src/jTunes/App.java
@@ -1,7 +1,8 @@
 package jTunes;
 
-import java.util.*;
+import java.util.List;
 import javax.swing.*;
+import java.awt.*;
 
 public class App extends JPanel {
     private static final String VersionID = "1.01";
@@ -11,6 +12,45 @@ public class App extends JPanel {
     public App() {
         OurDb db = new OurDb();
         choices = db.getGenres();
+        
+        setPreferredSize(new Dimension(800, 600));
+        GroupLayout layout = new GroupLayout(this);
+        setLayout(layout);
+        
+        JPanel[] menu = new JPanel[] {
+            new JPanel(),
+            new JPanel(),
+            new JPanel()
+        };
+        String[] menuItems = {
+                "Blues",
+                "Jazz",
+                "Rock n Roll"
+        };
+        
+        // filling, sizing, and coloring
+        for (int i = 0; i < menu.length; ++i) {
+            menu[i].setLayout(new BorderLayout());
+            menu[i].add(new JLabel(menuItems[i], JLabel.CENTER), BorderLayout.CENTER);
+            
+            menu[i].setPreferredSize(new Dimension(300, 50));
+            
+            menu[i].setBackground(Color.CYAN);
+        }
+        
+        // horizontal group
+        GroupLayout.ParallelGroup hGroup = 
+                layout.createParallelGroup(GroupLayout.Alignment.LEADING);
+        for (int i = 0; i < menu.length; ++i)
+            hGroup.addComponent(menu[i]);
+        layout.setHorizontalGroup(hGroup);
+        
+        // vertical group
+        GroupLayout.SequentialGroup vGroup =
+                layout.createSequentialGroup();
+        for (int i = 0; i < menu.length; ++i)
+            vGroup.addComponent(menu[i]);
+        layout.setVerticalGroup(vGroup);
     }
     
     public static void main(String[] args) {
@@ -18,6 +58,7 @@ public class App extends JPanel {
             public void run() {
                 JFrame f = new JFrame("jTunes" + VersionID);
                 f.add(new App());
+                f.setPreferredSize(new Dimension(300, 600));
                 f.pack();
                 f.setVisible(true);
             }
