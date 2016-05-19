49eaa41d1d91d9c1c56e85b5b8a4a1ec62726e3d
PowerstarL2 <joshua.chu@macaulay.cuny.edu>
2016-05-19 15:22:52 -0400

    The BodyPanel class is back.

---
+++ jTunes/src/jTunes/gui/BodyPanel.java
@@ -0,0 +1,11 @@
+package jTunes.gui;
+
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public class BodyPanel extends JPanel {
+    public BodyPanel() {
+        setPreferredSize(new Dimension(350, 500));
+    }
+}
