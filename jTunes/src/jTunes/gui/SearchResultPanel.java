6c9bcf98c003e916c9c99e56af8f8cb233444fda
Joshua Chu <joshua.chu@macaulay.cuny.edu>
2016-05-22 23:34:40 -0400

    SearchResultPanel commented

--- jTunes/src/jTunes/gui/SearchResultPanel.java
+++ jTunes/src/jTunes/gui/SearchResultPanel.java
@@ -16,6 +16,7 @@ import javax.swing.JScrollPane;
 public class SearchResultPanel extends JPanel {
     private JPanel resultsPane;
     // This class is the panel that contains all of the selections we need to search for songs.
+    // i.e., the results of database searches for genre, artist, etc. will appear here.
     public SearchResultPanel() {
         // Initializing the layout of the panel.
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
@@ -27,6 +28,7 @@ public class SearchResultPanel extends JPanel {
         resultsPane.setPreferredSize(new Dimension(350, 0));
         resultsPane.setBackground(Color.WHITE);
         // We will support vertical scrollbars if we have many elements.
+        // It works very nicely.
         JScrollPane scroll = new JScrollPane(
                 resultsPane,
                 JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
@@ -40,18 +42,23 @@ public class SearchResultPanel extends JPanel {
     // Function to add the search results from the Menu class, which gets them from the SQL server.
     public void addSearchResults(List<SearchResult> searchResults,
                                  QueryResponse response) {
+        // dynamic resizing of resultsPane to accommodate all search results
         Dimension d = resultsPane.getPreferredSize();
         d.setSize((int)d.getWidth(), searchResults.size() * 50);
         resultsPane.setPreferredSize(d);
         
+        // the last search result displays without a divider
         searchResults.get(searchResults.size() - 1).setLast(true);
+        
+        // add search results to resultsPane
+        // after giving them a way to respond if clicked.
         for (SearchResult s : searchResults) { 
             s.setResponse(response);
             resultsPane.add(s);
         }
     }
     
-    // This clears all of the result panes. 
+    // This clears all of the search results from our resultPane (read: our GUI)
     public void clearSearchResults() {
         resultsPane.removeAll();
         resultsPane.repaint();
