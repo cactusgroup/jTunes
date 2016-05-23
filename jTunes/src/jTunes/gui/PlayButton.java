10bf41c24f29c1af6fdbea2c0b44710dd6623501
Joshua Chu <joshua.chu@macaulay.cuny.edu>
2016-05-22 23:49:17 -0400

    PlayButton commented

--- jTunes/src/jTunes/gui/PlayButton.java
+++ jTunes/src/jTunes/gui/PlayButton.java
@@ -12,7 +12,7 @@ import jTunes.database.MP3Player_GUI;
 @SuppressWarnings("serial")
 // This function deals with the Play button in our project. This activates playback and pausing
 public class PlayButton extends CustomButton {
-    private boolean playing; // boolean to kee track of playing song.
+    private boolean playing; // boolean to keep track of playing song.
     
     // Constructor for playbutton.
     public PlayButton() {
@@ -22,7 +22,7 @@ public class PlayButton extends CustomButton {
               Color.LIGHT_GRAY,
               new Insets(0,1,0,1));
         
-        playing = false;// initialize that playing is not active.
+        playing = false; // initialize that playing is not active.
     }
     
     // our toggle function.
@@ -31,16 +31,20 @@ public class PlayButton extends CustomButton {
             setIcon(Resources.pauseIcon, "||", 40);
             MP3Player_GUI.play(); // Play the song.
             
-            // revert to play icon on song end
-            // and prepare to play again.
-            Timer t = new Timer(0, null); // Setting a timer to keep track of automated measurement
+            // Setting a timer to repeatedly test completion condition
+            // without overwhelming the event handling thread with a loop
+            Timer t = new Timer(0, null);
             ActionListener a = new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
-                    if (MP3Player_GUI.isCompleted()) { // If the song played all the way to the end,
+                    // On song end, revert to play icon
+                    // and prepare to play again.
+                    if (MP3Player_GUI.isCompleted()) {
                         PlayButton.this.setIcon(Resources.playIcon, ">", 40);
                         playing = !playing; // set that playing is not active
                         // and set the seek time back to the beginning. 
                         MP3Player_GUI.backtoBeginning();
+                        
+                        // stop the repetition.
                         t.stop();
                     }
                 }
@@ -50,7 +54,7 @@ public class PlayButton extends CustomButton {
             t.start();
         } else {                        // that is, Button clicked to pause song
             setIcon(Resources.playIcon, ">", 40); // Otherwise, set the icon to show pause.
-            MP3Player_GUI.pause();
+            MP3Player_GUI.pause(); // pause playback
         }
         playing = !playing; // toggle our boolean 
     }
