10294a8d72629a92786acd93a599b999204ef02c
ahmed3m <ahmed_3m2010@hotmail.com>
2016-05-22 20:32:10 -0400

    Fix MenuTest

--- jTunes/src/jTunes/database/MenuTest.java
+++ jTunes/src/jTunes/database/MenuTest.java
@@ -1,4 +1,5 @@
 package jTunes.database;
+import java.io.File;
 import java.net.URISyntaxException;
 import java.sql.SQLException;
 import java.util.Scanner;
@@ -11,9 +12,9 @@ public class MenuTest {
         final String select = "Please select a ";
         
         Scanner scanner = new Scanner(System.in);
-        String input = "";
-        String savedInput = "";
-        String savedGenre = "";
+        String input;
+        String savedInput;
+        String savedGenre;
         
         Menu menu = new Menu();
         System.out.println();
@@ -41,16 +42,21 @@ public class MenuTest {
         	        	continue;
         	        }
         	        else break;
-        		
         	}
-        if (exitBool) break; // Quitting the application if exitBool is triggered.
+        	
+        if (exitBool) 
+        	break; // Quitting the application if exitBool is triggered.
         savedInput = input;
         savedGenre = savedInput;
         
         while(true){
         	 System.out.println();
-             System.out.print(select + "artist (type its name): ");
+             System.out.print(select + "artist (type its name) or type 'quit' to exit: ");
              input = scanner.nextLine();
+             if(input.contentEquals("quit") || input.contentEquals("Quit")) {
+ 	        	exitBool = true;
+ 	        	break;
+ 	        }
              System.out.println(underline);
              if(menu.getAlbumsByArtistInGenre(savedGenre, input).isEmpty()){
             		System.out.println("Invalid Choice.");
@@ -60,21 +66,29 @@ public class MenuTest {
     	        }
     	        else break;	
         	}
+        if(exitBool)
+        	break;
         savedInput = input;
         
         while(true){
         	System.out.println();
-        	System.out.print(select + "album (type its name): ");
+        	System.out.print(select + "album (type its name) or type 'quit' to exit: ");
         	input = scanner.nextLine();
+        	if(input.contentEquals("quit") || input.contentEquals("Quit")) {
+ 	        	exitBool = true;
+ 	        	break;
+ 	        }
         	System.out.println(underline);
         	if(menu.getSongsInAlbumByArtistInGenre(input).isEmpty()){
         		System.out.println("Invalid Choice.");
         		System.out.println(underline);
-        		menu.getAlbumsByArtistInGenre(savedInput, savedGenre);
+        		menu.getAlbumsByArtistInGenre(savedGenre, savedInput);
 	        	continue;
         	}
         	else break;
         }
+        if(exitBool)
+        	break;
         savedInput = input;
         
         while(true){
@@ -85,7 +99,17 @@ public class MenuTest {
 	        	exitBool = true;
 	        	break;
 	        }
-        	System.out.println(underline);
+        	if(new File("src/mp3tracks/" + input + ".mp3").exists()) {
+        		menu.activateSong(input);
+        		break;
+        	}
+        	else {
+        		System.out.println(underline);
+        		System.out.println("Invalid Choice or file doesn't exist");
+        		System.out.println(underline);
+        		menu.getSongsInAlbumByArtistInGenre(savedInput);
+        		continue;
+        	}
         	/*
         	if (!menu.activateSong(input)){
         		System.out.println(underline);
@@ -94,12 +118,9 @@ public class MenuTest {
         	}
         	else break;
         	*/
-        	menu.activateSong(input);
-        	break;
         	}        	
         }
-        System.out.println("Exiting, goodbye!");
-        
+        System.exit(0);
         Runtime.getRuntime().addShutdownHook(new Thread() {
         	public void run() {
                 System.out.println("Exiting, goodbye!");
